03215a8a16f54b855cc97859f5388dfebe0cb075
Armando Salazar <armandosalazar351@gmail.com>
2023-02-12 22:09:41 -0600

    Initial commit

---
+++ src/main/java/com/armandosalazar/sbcode/SbcodeApplication.java
@@ -0,0 +1,13 @@
+package com.armandosalazar.sbcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/armandosalazar/sbcode/SbcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armandosalazar.sbcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
